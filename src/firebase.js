e1cf52b54fbfabfd88f796c1836fd55f71c72223
FarrukhGit <1803910@brunel.ac.uk>
2021-02-10 19:14:40 +0000

    Defining the sidebar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfPRDQPYHfBoQZkrKoOH2Vjgydgi4-dxk",
+  authDomain: "whatsapp-mern-33717.firebaseapp.com",
+  projectId: "whatsapp-mern-33717",
+  storageBucket: "whatsapp-mern-33717.appspot.com",
+  messagingSenderId: "739897651460",
+  appId: "1:739897651460:web:e38b93d3530ae9b5242051",
+  measurementId: "G-ZX44CT2GBZ"
+};

f53d8b4893a5a0ad697d3b2bf7e9b44957199441
FarrukhGit <1803910@brunel.ac.uk>
2021-02-10 19:49:56 +0000

    Created a chat header section

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import './Chat.css'
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Chat.css";
+import { IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AddCommentIcon from "@material-ui/icons/AddComment";
 
 function Chat() {
-    return (
-        <div className='chat'>
-            <h3>I am chat</h3>
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar />
+        <div className="chat_headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
         </div>
-    )
+
+        <div className="char_headerRight">
+          <IconButton>
+            <AddCommentIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat_body"></div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;

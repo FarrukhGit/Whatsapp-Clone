9a6574ea3aa71ceab6a88e75e0b93d21e3abbd7a
FarrukhGit <1803910@brunel.ac.uk>
2021-02-11 09:09:44 +0000

    Designing the footer

--- src/Chat.js
+++ src/Chat.js
@@ -5,6 +5,8 @@ import { IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AddCommentIcon from "@material-ui/icons/AddComment";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
   return (
@@ -39,6 +41,15 @@ function Chat() {
           This is a message
           <span className="chat_timestamp">{new Date().toUTCString()}</span>
         </p>
+      </div>
+
+      <div className="chat_footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
